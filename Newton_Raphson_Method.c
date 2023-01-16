703eae22d1078875171326e7ca05bd8cc2d020e4
Arnab Das Joy <bsse1308@iit.du.ac.bd>
2023-01-16 22:14:22 +0600

    Primary code, has a lot of bugs.

---
+++ Newton_Raphson_Method.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define F(x) (x*x - 2)
+#define dF(x) (2*x)
+#define initialGuess 1.0
+#define tol 1e-20
+
+int main(void){
+    double x1 = initialGuess, fx1 = F(x1), x2;
+
+    for(int i=0; i<10; ++i){
+        x2 = x1 - F(x1)/dF(x1);
+        double fx2 = F(x2);
+
+        printf("itr: %d\t\tprevx: %lf\t\tnewx: %lf\t\tprevF: %lf\t\tnewF: %lf\t\tInterval: %lf\t\t", i+1, x1, x2, F(x1), F(x2), abs(fx1-fx2));
+        printf("\n");
+
+        if(abs(fx1-fx2) < tol)
+        {
+            printf("Root to the equation: %lf", x2);
+            return 0;
+        }
+
+        x1 = x2, fx1 = fx2;
+    }
+
+    return 0;
+}
+
